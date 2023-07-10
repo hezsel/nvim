f635d71de8475f908b03d8e68a6eab391eeb5688
Hezsel <vhesselp@gmail.com>
2023-07-10 13:44:25 -0300

    feat: lualine

--- after/plugin/treesj.lua
+++ after/plugin/treesj.lua
@@ -1,3 +1,9 @@
+local tsj = require('treesj')
+
+tsj.setup({
+  max_join_length = 500,
+})
+
 vim.keymap.set('n', '<leader>m', require('treesj').toggle)
 vim.keymap.set('n', '<leader>M', function()
   require('treesj').toggle({ split = { recursive = true } })
